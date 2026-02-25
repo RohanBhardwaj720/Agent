3f1196effa00bdbb20f2b85963519cf5578e0d30
Rohan Bhardwaj <rohanbhardwaj720@gmail.com>
2026-02-25 22:38:31 +0530

    tools added

--- langGraph6/backend.py
+++ langGraph6/backend.py
@@ -7,7 +7,10 @@ from dotenv import load_dotenv
 from langchain_core.messages import AnyMessage
 from langchain_core.messages import HumanMessage, AIMessage
 import uuid
+from langchain_community.tools import DuckDuckGoSearchRun
 import sqlite3
+from langchain.tools import tool
+from langgraph.prebuilt import ToolNode, tools_condition
 
 load_dotenv()
 
@@ -15,11 +18,21 @@ load_dotenv()
 class chatbot(TypedDict):
     messages: Annotated[list[AnyMessage], operator.add]
 
+search_tool = DuckDuckGoSearchRun(region="us-en")
+
+@tool 
+def add_two_numbers(a: int, b: int) -> int:
+    """Add two numbers together."""
+    return { "addition_result": a + b }
+
+@tool
+def multiply_two_numbers(a: int, b: int) -> int:
+    """Multiply two numbers together."""
+    return { "multiplication_result": a * b }
 
 def generate_thread_id():
     """Generate a new unique thread id using uuid4."""
     return str(uuid.uuid4())
-
 
 def list_threads():
     """Return a list of all thread ids from the checkpointer memory."""
@@ -28,7 +41,6 @@ def list_threads():
         for checkpoint in checkpointer.list(config=None)
         if "thread_id" in checkpoint.config['configurable']
     }
-
 
 def get_thread_messages(thread_id: str):
     """Return the state for a given thread id from the compiled graph."""
@@ -49,21 +61,33 @@ llm = AzureChatOpenAI(
     api_version="2024-12-01-preview"
 )
 
+llm_wth_tools = llm.bind_tools([search_tool, add_two_numbers, multiply_two_numbers])    
+
 def LLM(state: chatbot) -> chatbot:
-    response = llm.invoke(state["messages"])
-    return {"messages": [AIMessage(response.content)]}
+    response = llm_wth_tools.invoke(state["messages"])
+    return {"messages": [response]}
 
 graph = StateGraph(chatbot)
 graph.add_node("LLM", LLM)
+tools = ToolNode(tools=[search_tool, add_two_numbers, multiply_two_numbers])
+graph.add_node("tools", tools)
 graph.add_edge(START, "LLM")
-graph.add_edge("LLM", END)
+graph.add_conditional_edges("LLM", tools_condition, {"tools": "tools", END: END})
+graph.add_edge("tools", "LLM")
 
 conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
 checkpointer = SqliteSaver(conn=conn)
 g = graph.compile(checkpointer= checkpointer)
-# config = {"configurable": {"thread_id": "1"}}
 
-# g.invoke({"messages": [HumanMessage("hi")]}, config=config)
+# png_bytes = g.get_graph(xray=True).draw_mermaid_png()
+# with open("agent.png", "wb") as f:
+#     f.write(png_bytes)
+# print("Saved agent.png")
+
+# config = {"configurable": {"thread_id": "1"}}
+# display(Image(g.get_graph(xray=True).draw_mermaid_png()))
+
+# g.invoke({"messages": [HumanMessage("add 2 and 3")]}, config=config)
 # config = {"configurable": {"thread_id": "2"}}
 # g.invoke({"messages" :[HumanMessage("what is my name")]}, config=config)
 # print([checkpoint.config['configurable']['thread_id'] for checkpoint in checkpointer.list(config=None)])
---
+++ langGraph6/frontend.py
@@ -0,0 +1,66 @@
+import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage
+from backend import g, generate_thread_id, list_threads, get_thread_messages
+
+# if there is a set named threads in session state, use it, otherwise initialize it using list_threads() from backend 
+if "threads" not in st.session_state:
+    st.session_state["threads"] = list_threads()
+
+def create_new_thread():
+    new_thread_id = generate_thread_id()
+    st.session_state["threads"].add(new_thread_id)
+    st.session_state["current_thread"] = new_thread_id
+    st.session_state["chat_history"] = [] 
+
+
+# if current thread is not in session state, initialize it using generate_thread_id() from backend
+if "current_thread" not in st.session_state:
+   create_new_thread()
+
+# New chat button
+if st.sidebar.button("New Chat", key="new_chat_btn"):
+    create_new_thread()
+
+if "chat_history" not in st.session_state:
+    st.session_state["chat_history"] = get_thread_messages(st.session_state["current_thread"])
+
+# Show thread buttons
+for tid in st.session_state["threads"]:
+    if st.sidebar.button(f"Thread {tid[:8]}", key=f"thread_{tid}"):
+        st.session_state["current_thread"] = tid
+        st.session_state["chat_history"] = get_thread_messages(tid)
+
+st.title("Chatbot")
+
+# Show chat history for the current thread
+for msg in st.session_state["chat_history"]:
+    if msg.get("role") == "human":
+        with st.chat_message("user"):
+            st.write(msg['content'])
+    elif msg.get("role") == "ai":
+        with st.chat_message("assistant"):
+            st.write(msg['content'])
+    else:
+        continue
+
+prompt = st.chat_input('Type here')
+
+if prompt:
+    # Add the user's message to the chat history
+    st.session_state["chat_history"].append({"role": "human", "content": prompt})
+    with st.chat_message('user'):
+        st.text(prompt)
+
+    CONFIG = {'configurable': {'thread_id': st.session_state['current_thread']}}
+    with st.chat_message('assistant'):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk, metadata in g.stream(
+                {'messages': [HumanMessage(content=prompt)]},
+                config= CONFIG,
+                stream_mode= 'messages'
+            )
+            if isinstance(message_chunk, AIMessage)
+        )
+
+    st.session_state['chat_history'].append({'role': 'ai', 'content': ai_message})
